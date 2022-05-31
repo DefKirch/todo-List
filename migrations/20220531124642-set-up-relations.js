23da7636aa07c0abd3b04ff0326507333ec1fcd8
DEFKIRCH <thijmen.kirch@gmail.com>
2022-05-31 15:25:39 +0200

    set-up relations

--- migrations/20220531124642-set-up-relations.js
+++ migrations/20220531124642-set-up-relations.js
@@ -1,21 +1,30 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("todoLists", "userId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "users",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
+
+    await queryInterface.addColumn("todoItems", "todoListId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "todoLists",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL",
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("todoLists", "userId");
+    await queryInterface.removeColumn("todoItems", "todoListId");
+  },
 };
